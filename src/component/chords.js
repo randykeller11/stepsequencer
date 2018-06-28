864ee29effe0d68e196d262ac0faf5f07a0bb1b2
Thomas Gardiner <thomgardiner@gmail.com>
2018-06-28 14:16:44 -0600

    chord test

--- src/component/chords.js
+++ src/component/chords.js
@@ -12,39 +12,34 @@ class Chords extends Component {
         let time = Tone.now();
         console.log(Tone.now())
         return time;
-      }
+    }
       
-      
-      playExample = () => {
-      
+     
+    playExample = () => {
+
+        Tone.Transport.bpm.value = 120;
         let currentTime = this.getTime();
         
         var synth1 = new Tone.Synth().toMaster()
         var synth2 = new Tone.Synth().toMaster()
         var synth3 = new Tone.Synth().toMaster()
       
-        synth1.triggerAttackRelease('C4', '32n', currentTime + 0)
-        synth2.triggerAttackRelease('E4', '32n', currentTime + 0)
-        synth3.triggerAttackRelease('G4', '32n', currentTime + 0)
-        setTimeout(function(){console.log("set 1")})
-        setTimeout(function(){console.log("set 2")}, 1000)
-        setTimeout(function(){console.log("set 3")}, 2000)
-        setTimeout(function(){console.log("set 4")}, 3000)
+        synth1.triggerAttackRelease('C4', '1n')
+        synth2.triggerAttackRelease('E4', '1n')
+        synth3.triggerAttackRelease('G4', '1n')
       
+        synth1.triggerAttackRelease('G4', '1n', currentTime + 1.5)
+        synth2.triggerAttackRelease('B4', '1n', currentTime + 1.5)
+        synth3.triggerAttackRelease('D4', '1n', currentTime + 1.5)
+           
+        synth1.triggerAttackRelease('A4', '1n', currentTime + 3.0)
+        synth2.triggerAttackRelease('C4', '1n', currentTime + 3.0)
+        synth3.triggerAttackRelease('E4', '1n', currentTime + 3.0)
       
-        synth1.triggerAttackRelease('G4', '16n', currentTime + 1)
-        synth2.triggerAttackRelease('B4', '16n', currentTime + 1)
-        synth3.triggerAttackRelease('D4', '16n', currentTime + 1)
-        
-        
-        synth1.triggerAttackRelease('A4', '4n', currentTime + 2)
-        synth2.triggerAttackRelease('C4', '4n', currentTime + 2)
-        synth3.triggerAttackRelease('E4', '4n', currentTime + 2)
-      
-        synth1.triggerAttackRelease('C4', '8n', currentTime + 3)
-        synth2.triggerAttackRelease('E4', '8n', currentTime + 3)
-        synth3.triggerAttackRelease('G4', '8n', currentTime + 3)
-        
+        synth1.triggerAttackRelease('C4', '1n', currentTime + 4.5)
+        synth2.triggerAttackRelease('E4', '1n', currentTime + 4.5)
+        synth3.triggerAttackRelease('G4', '1n', currentTime + 4.5)
+
       }
 
     render(){
