4839b4bb313316c22e620ac88ac3339497420d09
Thomas Gardiner <thomgardiner@gmail.com>
2018-07-11 07:53:39 -0600

    working mongoDB connection

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
 // const User = require('./models/user.js');
 
-const dbURL = "mongodb://<dbuser>:<dbpassword>@ds233571.mlab.com:33571/daw"
+const dbURL = "mongodb://admin:password1@ds233571.mlab.com:33571/daw"
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -19,36 +19,29 @@ app.get('/ping', function (req, res) {
  return res.send('pong');
 });
 
-app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
-});
-
-
-
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
-
-// app.use(express.static("client"));
-
-// const mongoDB = process.env.MONGODB_URI || dbURL;
-// mongoose.connect(mongoDB);
-// mongoose.Promise = global.Promise;
-
-// const db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-// db.once('open', function() {
-//     console.log("connected to the mainframe");
+// app.listen(PORT, function() {
+//   console.log("App listening on PORT " + PORT);
 // });
 
-// app.use(session({
-//   secret: 'nic cage',
-//   resave: false,
-//   saveUninitialized: true,
-//   cookie: { secure: false },
-//   store: new MongoStore({
-//     mongooseConnection: db
-//   })
-// }))
+const mongoDB = process.env.MONGODB_URI || dbURL;
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', function() {
+    console.log("connected to the mainframe");
+});
+
+app.use(session({
+  secret: 'big burrito',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false },
+  store: new MongoStore({
+    mongooseConnection: db
+  })
+}))
 
 // console.log("after mongo but not connected");
 
@@ -57,8 +50,8 @@ app.listen(PORT, function() {
 // require('./routes/htmlRoutes.js')(app);
 // require('./routes/loginRoutes.js')(app);
 
-// console.log("after session stuff");
+console.log("after session stuff");
 
-// app.listen(PORT, function() {
-//   console.log("App listening on PORT " + PORT);
-// });
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
